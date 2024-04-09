5a26fee8167a3c72033331dc21ef22c9092ca56f
hite <695663633@qq.com>
2024-04-09 12:33:08 +0800

    增加一些服务器

--- vars.py
+++ vars.py
@@ -2,7 +2,13 @@ from typing import List, Dict
 
 servers: List[str] = ["s2.wemc.cc",  # 220.198.120.151
                       "cn-yw-plc-1.openfrp.top",  # 122.242.149.92
+                      "ningbo-3689d402.of-7af93c01.shop",
+                      "cn-hz-bgp-1.of-7af93c01.shop",
                       "cn-hk-bgp-4.openfrp.top",  # 191.96.240.164
+                      "ganzhou-eb7c59a5.of-7af93c01.shop",
+                      "cn-he-plc-2.of-7af93c01.shop",
+                      "cn-bj-plc-2.of-7af93c01.shop",
+                      "shenzhen-a8bf9282.of-7af93c01.shop",
                       "ipyingshe.com",  # 111.45.28.82
                       "xiaomy.net",  # 112.16.229.54
                       "yuming.net",  # 118.139.160.68
